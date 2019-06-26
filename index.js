2d008843d2b4d22656bb171b7ec778c8d4ce23dd
Jamie Moore <MooreJamiee@johndeere.com>
2019-06-26 16:24:53 -0500

    feat: initial project setup and implementation of knn

---
+++ index.js
@@ -0,0 +1,28 @@
+import loadCSV from './load-csv'
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs');
+import knn from './knn';
+
+let {features, labels, testFeatures, testLabels} = loadCSV('kc_house_data.csv', {
+  shuffle: true,
+  splitTest: 10,
+  dataColumns: ['lat', 'long', 'sqft_lot', 'sqft_living'],
+  labelColumns: ['price']
+});
+
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+
+testFeatures.forEach((testPoint, i) => {
+
+  const result = knn(features, labels, tf.tensor(testPoint), 10);
+  const error = (testLabels[i][0] - result) / testLabels[i][0];
+
+  console.log('error: ', error * 100);
+
+});
+
+
+
+
+
