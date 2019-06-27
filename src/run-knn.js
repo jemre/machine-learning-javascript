159c842f273f133c40943538f697d6e41ac9fc7d
Jamie Moore <MooreJamiee@johndeere.com>
2019-06-27 12:55:33 -0500

    refactor: extract knn running script to new file

---
+++ src/run-knn.js
@@ -0,0 +1,31 @@
+import loadCSV from './utilities/load-csv';
+import knn from './algorithms/knn';
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs');
+
+const runKNN = () => {
+
+    let {features, labels, testFeatures, testLabels} = loadCSV('src/data-sets/kc_house_data.csv', {
+        shuffle: true,
+        splitTest: 10,
+        dataColumns: ['lat', 'long', 'sqft_lot', 'sqft_living'],
+        labelColumns: ['price']
+    });
+
+    features = tf.tensor(features);
+    labels = tf.tensor(labels);
+
+    testFeatures.forEach((testPoint, i) => {
+
+        const result = knn(features, labels, tf.tensor(testPoint), 10);
+        const error = (testLabels[i][0] - result) / testLabels[i][0];
+
+        console.log('error: ', error * 100);
+
+    });
+
+};
+
+export default runKNN;
+
+
